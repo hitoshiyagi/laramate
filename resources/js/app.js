26d7a24149ccb3e039e9f9f0fa41a33dae9d81eb
yagi-web <223397855+yagi-web@users.noreply.github.com>
2026-01-03 17:09:23 +0900

    Add Vite build assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import "../css/app.css"; // CSS を Vite 経由で読み込む
+
+// もし Bootstrap や axios などを使っている場合
+import "bootstrap";
+import axios from "axios";
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,6 @@
 @section('content_header')
 @stop
 
-@section('css')
-@vite('resources/css/app.css')
-{{-- 必要なら追加のカスタムCSS --}}
-{{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
-
 @section('content')
 <h1>Welcome to this Laramate.</h1>
 
@@ -19,19 +13,18 @@
     <div class="card mt-10" id="project-card">
         <div class="card-header bg-primary">Laramate（ララメイト）とは？</div>
         <div class="card-body">
-            <p>
-                Laramateは、Laravel開発における「命名の迷い」をなくすためのサポートアプリです。<br>
+            <p>Laramateは、Laravel開発における「命名の迷い」をなくすためのサポートアプリです。<br>
                 プロジェクトや要素名を自動生成し、統一された命名規則でコードの可読性とチームの生産性を高めます。<br><br>
                 このアプリは、「命名作業に時間を取られる」「命名ルールがバラバラで混乱する」といった課題を解決したいという想いから生まれました。<br>
                 自分自身の課題を出発点に、同じ悩みを持つ開発者の力になれるように――<br>
-                Laramateはそんな想いで作られた、開発者の“相棒（mate）”です。
-            </p>
+                Laramateはそんな想いで作られた、開発者の“相棒（mate）”です。</p>
         </div>
     </div>
 </div>
 @stop
 
 @section('js')
-@vite('resources/js/element.js')
-@vite('resources/js/project.js')
+@vite('resources/js/app.js')
+<script src="{{ asset('js/element.js') }}"></script>
+<script src="{{ asset('js/project.js') }}"></script>
 @stop
